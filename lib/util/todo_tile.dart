8668eb692bcd6a47229856fbe56ff0bfc2fc1dbc
codewithjalloh <codewithjalloh@gmail.com>
2024-05-07 20:05:30 +0100

    update todo file

--- lib/util/todo_tile.dart
+++ lib/util/todo_tile.dart
@@ -1,1 +1,66 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
+class TodoTile extends StatelessWidget {
+  final String taskName;
+  final bool taskComplete;
+  Function(bool?)? onChange;
+  Function(BuildContext)? deleteFunction;
+
+  TodoTile({
+    super.key,
+    required this.taskName,
+    required this.taskComplete,
+    required this.onChange,
+    required this.deleteFunction,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(
+        left: 25.0,
+        right: 25.0,
+        top: 25.0,
+      ),
+      child: Slidable(
+        endActionPane: ActionPane(motion: StretchMotion(), children: [
+          SlidableAction(
+            borderRadius: BorderRadius.circular(12),
+            icon: Icons.delete,
+            onPressed: deleteFunction,
+            backgroundColor: Colors.grey,
+          )
+        ]),
+        child: Container(
+          padding: EdgeInsets.all(24),
+          decoration: BoxDecoration(
+            color: Colors.grey.shade300,
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Row(
+            children: [
+              // checkbox
+              Checkbox(
+                value: taskComplete,
+                onChanged: onChange,
+                activeColor: Colors.black,
+              ),
+
+              // task me
+              Text(
+                taskName,
+                style: TextStyle(
+                    decoration: taskComplete
+                        ? TextDecoration.lineThrough
+                        : TextDecoration.none),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
